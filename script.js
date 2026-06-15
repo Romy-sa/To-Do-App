6689b998f09d12f73660c742d1982cc6109c7f89
Romysa <romysaalfatig@gmail.com>
2026-06-15 21:28:02 +0200

    added light mode layout and functionality

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const itemsCount = document.getElementById("items-count");
+const toggleModeBtn = document.getElementById("toggle-mode");
 
 
 updateItemsCount();
 
+toggleModeBtn.addEventListener("click", () => {
+    if(document.body.classList.contains("light-mode")) {
+        document.body.classList.remove("light-mode");
+        toggleModeBtn.innerHTML = `<img src="./images/icon-sun.svg">`;
+    } else {
+        document.body.classList.add("light-mode");
+        toggleModeBtn.innerHTML = `<img src="./images/icon-moon.svg">`
+    }
+});
 
 function updateItemsCount() {
     count = document.querySelectorAll(".list-container .todo-item").length;
